3ac9ee5e32710a565191ae54ac7f9a6116fd4659
maogpt <kelikejitech@gmail.com>
2023-11-25 16:39:45 +0800

    feat: Return greetings for multiple people

--- tutorial/Create-a-Go-module/hello/hello.go
+++ tutorial/Create-a-Go-module/hello/hello.go
@@ -14,13 +14,20 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	// Request a greeting message.
-	message, err := greetings.Hello("xhm")
+	// A slice of names.
+	names := []string{
+		"Gladys", "Samantha", "Darrin",
+	}
+
+	// Request greeting messages for the names.
+	message, err := greetings.Hellos(names)
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
 		log.Fatal(err)
 	}
+	// If no error was returned, print the returned map of
+	// messages to the console.
 	fmt.Println(message)
 }
 
